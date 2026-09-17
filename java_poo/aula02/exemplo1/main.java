b00b60d4cf783b3a325cc06fba79a845752291b2
Raphael <raphael.mauricio@gmail.com>
2026-09-17 13:26:51 -0300

    extra

--- java_poo/aula02/exemplo1/main.java
+++ java_poo/aula02/exemplo1/main.java
@@ -1,6 +1,6 @@
 package java_poo.aula02.exemplo1;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Aluno aluno = new Aluno();
         aluno.inserirNome("Raphael");
